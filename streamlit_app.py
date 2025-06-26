f41aae21d9b576f2be3d7fb0f71ee9b80a48f152
anusha680 <anusha@synthlake.com>
2025-06-26 17:20:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 ingredients_list= st.multiselect(
@@ -37,6 +38,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
         sf_df=st.dataframe(smoothiefroot_response.json(),use_container_width=True)
+        
 
     # st.write(ingredients_string)
 
